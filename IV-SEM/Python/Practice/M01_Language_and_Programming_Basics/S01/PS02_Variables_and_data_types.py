d9abdad880be476b5f4cefa815eacda6705d9ba3
Anvikha <anvikhasalivendra@gmail.com>
2026-01-07 09:59:08 +0530

    Structure Upload

---
+++ IV-SEM/Python/Practice/M01_Language_and_Programming_Basics/S01/PS02_Variables_and_data_types.py
@@ -0,0 +1,86 @@
+"""
+Data types in Python:
+1. Fundamental DT 
+1. int
+    2. float
+    3. complex
+    4. Boolean
+2. Non-Fundamental DT
+    1. str
+    2. list
+    3. tuple
+    4. set
+    5. dict
+Docstring for IV th sem.Python.practice.M01_Language_and_Programming_Basics.S01.PS02_Variables_and_data_types
+"""
+#practice problem: Variables and data types
+x = 10          # int
+y = 3.14        # float
+z = 2 + 3j     # complex
+is_valid = True # Boolean
+name = "Alice" # str
+numbers = [1, 2, 3, 4, 5] # list
+coordinates = (10.0, 20.0) # tuple
+unique_numbers = {1, 2, 3, 4, 5} # set
+person = {"name": "Alice", "age": 30} # dict
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
+print(type(name))
+print(type(numbers))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
+
+
+x = 10          # int
+y = 3.14        # float
+z = 2 + 3j     # complex
+is_valid = True # Boolean
+name = "Alice" # str
+numbers = [1, 2, 3, 4, 5] # list
+coordinates = (10.0, 20.0) # tuple
+unique_numbers = {1, 2, 3, 4, 5} #
+person = {"name": "Alice", "age": 30} # dict
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
+print(type(name))
+print(type(numbers))
+print(type(coordinates))        
+print(type(unique_numbers))
+print(type(person))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
+print(type(name))
+print(type(numbers))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
+print(type(name))
+print(type(numbers))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
+print(type(name))
+print(type(numbers))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(is_valid))
