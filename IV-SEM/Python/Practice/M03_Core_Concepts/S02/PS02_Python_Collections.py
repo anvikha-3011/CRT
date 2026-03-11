85ee4ef8463a9ff06a64e288a808066f560eea63
Anvikha <anvikhasalivendra@gmail.com>
2026-03-11 09:18:53 +0530

    assignments completed

---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collections.py
@@ -0,0 +1,27 @@
+#1)
+
+
+
+#2 
+
+
+#3
+
+
+# 4)
+
+
+#5) Removing Elements from a List
+a=[10,20,30,40,50]
+a.remove(40)
+print(a)
+a.pop()
+print(a)
+
+#6) Slicing
+a=[10,20,30,40,50]
+print(a[0:])
+print(a[2:])
+#reverse the list using slicing
+
+#7) Leetcode compl
