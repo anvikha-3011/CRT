eb0f1032c41bbeb3bede3138b27c6d5ae0c7270b
Anvikha <anvikhasalivendra@gmail.com>
2026-03-11 09:32:48 +0530

    done

--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,5 +1,6 @@
 import math
 
+#Task
 def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
     # Calculate average
     avg = (n1 + n2 + n3) / 3
--- IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST05/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST05/task.py
@@ -1,5 +1,5 @@
 from typing import List
-
+#task
 
 def Collatz_Sequence(n: int) -> List[int]:
     sequence = [n]
